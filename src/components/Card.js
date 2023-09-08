349c4cf4a7ebfeeeedb137821f5a0a15d29dbb35
omkeshborse <bomkes25@gmail.com>
2023-09-08 08:35:36 +0530

    fixed : Card Title fixed

--- src/components/Card.js
+++ src/components/Card.js
@@ -17,7 +17,7 @@ export const Card = ({ movie }) => {
       <div className="p-5">
         <Link to={`/movie/${id}`}>
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            {title}1
+            {title}
           </h5>
         </Link>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
